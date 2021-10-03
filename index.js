d2ee99d152117e98e71c955d46e55becfbf5ae14
Camila Conde <camila.conde.lorente@gmail.com>
2021-10-03 17:22:17 -0400

    resolving entry issues

--- index.js
+++ index.js
@@ -1,39 +1,54 @@
-const lefToRightDiagonal = (arr) => {
-  const array = [];
-  let lefToRight = 0;
-  const n = Math.sqrt(arr.length);
+function diagonalDifference(arr) {
+  // Write your code here
+  const simpleArray = [];
 
-  for (let i = 0; i < arr.length; i = i + n + 1) {
-    array.push(arr[i]);
-    lefToRight += arr[i];
+for (let i = 0; i<arr.length; i++) {
+
+  for(let j = 0; j<arr[i].length; j++) {
+    simpleArray.push(arr[i][j]);
   }
+}
+ let leftSum =  lefToRightDiagonal(simpleArray);
+ let rightSum = rightToLeftDiagonal(simpleArray);
 
-  return lefToRight;
-};
+ if(leftSum > rightSum) {
+     return leftSum - rightSum;
+ } 
 
-const rightToLeftDiagonal = (arr) => {
+ else {
+     return rightSum - leftSum;
+
+ }
+
+}
+
+const lefToRightDiagonal = (simpleArray) => {
+     const array = [];
+     let lefToRight = 0;
+     const n = Math.sqrt(simpleArray.length);
+
+      for(let i = 0; i < simpleArray.length; i = i+n+1) {
+           array.push(simpleArray[i]);
+           lefToRight += simpleArray[i];
+      }
+
+      return lefToRight;
+
+}
+
+const rightToLeftDiagonal = (simpleArray) => {
   const array2 = [];
   let rightToLeft = 0;
-  const n = Math.sqrt(arr.length);
+  const n = Math.sqrt(simpleArray.length);
 
-  for (let i = n - 1; i < arr.length - 1; i = i + n - 1) {
-    array2.push(arr[i]);
-    rightToLeft += arr[i];
+  for(let i = n-1; i < simpleArray.length -1; i = i + n - 1) {
+      array2.push(simpleArray[i]);
+      rightToLeft += simpleArray[i];
+
   }
 
   return rightToLeft;
-};
-
-function diagonalDifference(arr) {
-  // Write your code here
-  let leftSum = lefToRightDiagonal(arr);
-  let rightSum = rightToLeftDiagonal(arr);
-
-  if (leftSum > rightSum) {
-    return leftSum - rightSum;
-  } else {
-    return rightSum - leftSum;
-  }
 }
+
 
 module.exports = diagonalDifference;
--- test.spec.js
+++ test.spec.js
@@ -1,19 +1,19 @@
 const diagonalDifference = require("./index");
 
-describe ("sums both diagonals", () => {
-  it("tests for square 3 matrix", () => {
-    const level = diagonalDifference([11, 2, 4, 4, 5, 6, 10, 8, -12]); 
-    expect(level).toStrictEqual(15);
-  });
+ describe ("sums both diagonals", () => {
+   it("tests for square 3 matrix", () => {
+     const level = diagonalDifference([ [ 11, 2, 4 ], [ 4, 5, 6 ], [ 10, 8, -12 ] ]); 
+     expect(level).toStrictEqual(15);
+   });
 
-  it("tests for square 4 matrix", () => {
-    const level = diagonalDifference([1, 4, 5, 6, 7, 9, 10, 12, 15, -4, -3, 2, 8, 20, -1, 6]); 
-    expect(level).toStrictEqual(7);
-  });
+   it("tests for square 4 matrix", () => {
+     const level = diagonalDifference([ [1, 4, 5, 6,], [7, 9, 10, 12], [15, -4, -3, 2], [8, 20, -1, 6] ]); 
+     expect(level).toStrictEqual(7);
+   });
 
-  it("tests for square 3 matrix", () => {
-    const level = diagonalDifference([10, -2, 7, 4, 1, 7, 3, 20, 2, 11, 6, 13, 5, 30, 25, -5, 3, 4, 8, 32, 4, 9, 7, 2, 15]); 
-    expect(level).toStrictEqual(26);
-  });
+   it("tests for square 5 matrix", () => {
+     const level = diagonalDifference([ [10, -2, 7, 4, 1], [7, 3, 20, 2, 11], [6, 13, 5, 30, 25], [-5, 3, 4, 8, 32], [4, 9, 7, 2, 15] ]); 
+     expect(level).toStrictEqual(26);
+   });
 
-})
+ })
